cffa449732d989307b9a3403bebb2c9f5049b8c2
itsPeetah <pietro.g.moroni@gmail.com>
2022-02-15 22:43:04 +0100

    Update ws-index.js

--- server/ws-index.js
+++ server/ws-index.js
@@ -4,6 +4,7 @@ const main = async () => {
   const wss = new WebSocketServer({ path: "/echo", port: 4000 });
 
   wss.on("connection", (ws) => {
+    console.log(ws);
     ws.on("message", (data) => {
       console.log("Received:", data.toString());
       ws.send(data.toString());
